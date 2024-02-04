d800a50b632b197dbf88f947dcb2e971be43fa3a
JbanzCodingLabz <jacob.r.bananal@outlook.com>
2024-02-04 14:59:21 -0800

    LoginWpfMVVM - Added LoginWpfMVVM.Data class, added LoginUser.cs, SQLQueryBase.cs, IQueryLoginWpfMVVM.cs, QueryLoginWpfMVVM.cs

---
+++ LoginWpfMVVM.Data/Helper/SQLQueryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginWpfMVVM.Data.Helper
+{
+    public abstract class SQLQueryBase
+    {
+        private readonly string m_connectionString = string.Empty;
+        public SQLQueryBase()
+        {
+            m_connectionString = "Server=JBANZPC\\SQLEXPRESS;Database=LoginMVVM;Trusted_Connection=True;";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(m_connectionString);
+        }
+    }
+}
---
+++ LoginWpfMVVM.Data/QueryLoginWpfMVVM/QueryLoginWpfMVVM.cs
@@ -0,0 +1,32 @@
+﻿using LoginWpfMVVM.Data.Helper;
+using LoginWpfMVVM.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginWpfMVVM.Data.QueryLoginWpfMVVM
+{
+    public class QueryLoginWpfMVVM : SQLQueryBase, IQueryLoginWpfMVVM
+    {
+        private static QueryLoginWpfMVVM m_SingletonInstance = null;
+        public static QueryLoginWpfMVVM Instance
+        {
+            get
+            {
+                if (m_SingletonInstance == null) 
+                {
+                    m_SingletonInstance = new QueryLoginWpfMVVM();
+                }
+
+                return m_SingletonInstance;
+            }
+        }
+
+        private QueryLoginWpfMVVM()
+        {
+
+        }
+    }
+}
